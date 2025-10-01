5ecf18c727e5e5d95d9e6130f0cada1acb903240
Andrew McElvey <andrew.mcelvey@gmail.com>
2025-10-01 13:16:16 -0500

    Add files via upload

---
+++ 10-1-25/starter/BankingApp.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class BankingApp {
+	
+	private static ArrayList<BankAccount> loadAccounts(ArrayList<String> names, 
+			ArrayList<Double> checking, ArrayList<Double> savings) 
+	{
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO: call FileUtils to get data from names, checking, savings
+		
+		// TODO: convert primitive arrays to Lists
+		
+		// TODO: call loadAccounts passing all three Lists
+		
+		// TODO: display all account information (a la 9.21)
+		
+		// TODO: transfer $100 from checking to savings within each object
+		
+		// TODO: display all account information (a la 9.21)
+
+	}
+
+}
---
+++ 10-1-25/starter/CollectionUtils.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class CollectionUtils {
+	public static ArrayList<Double> arrayToList(double[] array) {
+		return null;
+	}
+	
+	public static ArrayList<String> arrayToList(String[] array) {
+		return null;
+	}
+	
+}
